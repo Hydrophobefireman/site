5c47122c7bd64a8bc4665da242fbe0f26317cd0b
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2023-12-23 22:37:43 -0500

    finally

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -2,10 +2,26 @@ import {useEffect} from "@hydrophobefireman/ui-lib";
 
 const BLOG_URL = "https://blog.hpfm.dev";
 const BLOG_DOMAIN = new URL(BLOG_URL).host;
+if (!Promise.prototype.finally) {
+  Promise.prototype.finally = function (callback) {
+    if (typeof callback !== "function") {
+      return this.then(callback, callback);
+    }
+    const P = this.constructor || Promise;
+    return this.then(
+      (value) => P.resolve(callback()).then(() => value),
+      (err) =>
+        P.resolve(callback()).then(() => {
+          throw err;
+        }),
+    );
+  };
+}
 export default function Blog() {
   useEffect(() => {
-    (window as any).umami.track("blog opened");
-    location.href = BLOG_URL;
+    (window as any).umami.track("blog opened").finally(() => {
+      location.replace(BLOG_URL);
+    });
   }, []);
   return (
     <div>
